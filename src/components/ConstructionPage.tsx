653eb78458ad33295f5b04389e0d3da68a4c5da7
niserosp <niserosp@gmail.com>
2020-09-24 20:36:36 +0100

    Revert "code splitting"
    This reverts commit 60b21d6465e315898fedc4c39724bc4a7e7cdcfb.

--- src/components/ConstructionPage.tsx
+++ src/components/ConstructionPage.tsx
@@ -1,13 +1,12 @@
 import { Box } from 'grommet'
-import React, { Suspense, useState } from 'react'
+import React, { useState } from 'react'
 import { useHover } from 'react-use-gesture'
 import ConstructionTextAnimation from './animations/ConstructionTextAnimation'
+import Emojis from './animations/svgElementAnimations/EmojiAnimation'
+import Waves from './animations/svgElementAnimations/WavesAnimation'
 import CenterBox from './CenterBox'
-import { SkewLoader } from 'react-spinners'
 
 export default function () {
-    const LazyWaves = React.lazy(() => import('./animations/svgElementAnimations/WavesAnimation'))
-    const LazyEmojis = React.lazy(() => import('./animations/svgElementAnimations/EmojiAnimation'))
     const [hovering, hoverBindings] = useHovering()
 
     const constructionTextStyle = displayHidden(hovering)
@@ -15,15 +14,13 @@ export default function () {
     return (
         <CenterBox>
             <Box width='large' height='medium' align='center' as='main' {...hoverBindings()}>
-                <Suspense fallback={<SkewLoader color='white' />}>
-                    <svg viewBox='-25 -75 200 200' transform='matrix(1, 0.2, 0.2, 1, 0, 0)'>
-                        <LazyWaves pullApart={hovering} />
-                        <g transform='translate(4, -18)'>
-                            <LazyEmojis active={hovering} />
-                        </g>
-                    </svg>
-                    <ConstructionTextAnimation style={constructionTextStyle} />
-                </Suspense>
+                <svg viewBox='-25 -75 200 200' transform='matrix(1, 0.2, 0.2, 1, 0, 0)'>
+                    <Waves pullApart={hovering} />
+                    <g transform='translate(4, -18)'>
+                        <Emojis active={hovering} />
+                    </g>
+                </svg>
+                <ConstructionTextAnimation style={constructionTextStyle} />
             </Box>
         </CenterBox>
     )
