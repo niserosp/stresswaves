6a7c70326227b60bbabe34a99fdb448284bd3de1
niserosp <niserosp@gmail.com>
2020-09-27 15:45:46 +0100

    tweak(ui): test page padding

--- src/components/routes/AnnotationsTestPage.tsx
+++ src/components/routes/AnnotationsTestPage.tsx
@@ -6,7 +6,7 @@ import CenterBox from '../CenterBox'
 export default () => {
     return (
         <CenterBox>
-            <Box margin='small'>
+            <Box margin='large'>
                 <Heading>Annotations Test Page</Heading>
                 <Paragraph>Audio annotations are areas of a page where a user may interact to hear audio. Usually they will be used for text, but not necessarily always. The purpose of this page is to test implementations of audio annotations.</Paragraph>
 
