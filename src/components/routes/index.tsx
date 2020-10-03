369fe67917465e98ad289cee6a91f9d473ceb6c0
niserosp <niserosp@gmail.com>
2020-10-03 22:23:51 +0100

    cleanup(dev): delete annotations test page

--- src/components/routes/index.tsx
+++ src/components/routes/index.tsx
@@ -3,14 +3,12 @@ import { CircleLoader } from 'react-spinners'
 import { Route, Switch } from 'react-router-dom'
 import CenterBox from '../CenterBox'
 
-const AnnotationsTestPage = React.lazy(() => import('./AnnotationsTestPage'))
 const ConstructionPage = React.lazy(() => import('./ConstructionPage'))
 
 export default () => {
     return (
         <Suspense fallback={<CenterBox><CircleLoader color='white' /></CenterBox>}>
             <Switch>
-                <Route path='/annotations-test' component={AnnotationsTestPage} />
                 <Route path='/' component={ConstructionPage} exact />
             </Switch>
         </Suspense>
