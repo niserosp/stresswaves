da9f7c47e2c5c68bff1b7795cda75c264bb2fa22
niserosp <niserosp@gmail.com>
2020-12-24 14:57:30 +0000

    feature(page): random 'about' info

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,33 @@
+import React from 'react';
 import Main from '../shared/Main';
+import { chooseRandom } from '../shared/random';
 import { Title, Text } from '../shared/typography';
 
 export default function About() {
 	return (
 		<Main>
 			<Title>About</Title>
-			<Text>Now, her name is Stresswaves and it is night.</Text>
-			<Text> Don't take it any further than that. There's nothin' good about it.</Text>
+			<RandomAboutInfo />
 		</Main>
 	);
 }
+
+const RandomAboutInfo = () => {
+	const AboutInfo = chooseRandom(aboutInfoComponents);
+	return <AboutInfo />;
+};
+
+const LynchianAbout = () => {
+	return (
+		<React.Fragment>
+			<Text>Now, her name is Stresswaves and it is night.</Text>
+			<Text>Don't take it any further than that. There's nothin' good about it.</Text>
+		</React.Fragment>
+	);
+};
+
+const FlashAnimationAbout = () => {
+	return <Text>Stresswaves is a frame from a 2004 flash animation.</Text>;
+};
+
+const aboutInfoComponents = [ LynchianAbout, FlashAnimationAbout ];
